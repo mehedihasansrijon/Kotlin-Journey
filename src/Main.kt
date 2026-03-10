b787bccc97768b0780adfe02342882185312ef89
Mehedi Hasan Srijon <srijon.the.dreamer@gmail.com>
2026-03-10 12:37:14 +0600

    Add examples of Kotlin assignment operators with reusable print function

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,25 @@
-import models.Person
-
 fun main() {
-    val name = "Jhon Doe"
-    val birthYear = 1970
-    val latter = 'D'
-    val number = 41
-    val doubleNumber = 5.99
-    val isAlive = true
+    var priceAdd = 100
+    priceAdd += 10 // priceAdd = priceAdd + 10
+    showLabel("After +=", priceAdd)
+
+    var priceSub = 100
+    priceSub -= 10 // priceSub = priceSub - 10
+    showLabel("After -=", priceSub)
+
+    var priceMul = 100
+    priceMul *= 10 // priceMul = priceMul * 10
+    showLabel("After *=", priceMul)
+
+    var priceDiv = 100
+    priceDiv /= 10 // priceDiv = priceDiv / 10
+    showLabel("After /=", priceDiv)
+
+    var priceMod = 100
+    priceMod %= 10 // priceMod = priceMod % 10
+    showLabel("After %=", priceMod)
+}
+
+fun showLabel (labelName: String, value: Int) {
+    println("$labelName : $value")
 }
