ef60180be05d70962cfdc356d67989a2902ae7f6
Mehedi Hasan Srijon <srijon.the.dreamer@gmail.com>
2026-03-10 13:31:06 +0600

    Kotlin when

--- src/Main.kt
+++ src/Main.kt
@@ -1,30 +1,16 @@
 fun main() {
-    val balance = 81673 // Balance for this user
-    val withdrawAmount = 23500 // Withdraw amount for this user
-    val dailyLimit = 50000 // Daily limit
+    val day = 5
 
-    if (withdrawAmount <= 0) {
-        result("Invalid withdrawal amount") // Invalid withdrawal amount
-    } else if (withdrawAmount > dailyLimit) {
-        result("Daily withdrawal limit exceeded") // Daily withdrawal limit exceeded
-    } else if (withdrawAmount % 500 != 0) {
-        result("ATM can dispense only 500 or 1000 multiples") // ATM can dispense only 500 or 1000 multiples
-    } else if (withdrawAmount > balance) {
-        result("Insufficient balance") // Balance is Insufficient balance
-    } else {
-        val remainingBalance = balance - withdrawAmount // Balance remaining
-
-        val oneThousand = withdrawAmount / 1000 // One thousand notes count
-        val remaining = withdrawAmount % 1000
-        val fiveHundredNotes = remaining / 500 // five Handed notes count
-
-        result(
-            "Withdrawal successful\n" + "Remaining balance: ${remainingBalance}\nNotes:\n1000 * $oneThousand\n500 * $fiveHundredNotes"
-        )
+    val result = when (day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> "Invalid day."
     }
-}
 
-// Result showing method
-fun result(value: String) {
-    println(value)
+    println(result)
 }
