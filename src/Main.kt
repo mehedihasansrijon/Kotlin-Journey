b8f5d6fe9eb408c731a34404ed8c507541150e2e
Mehedi Hasan Srijon <srijon.the.dreamer@gmail.com>
2026-03-10 12:38:22 +0600

    Bolean

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,6 @@
 fun main() {
-    var priceAdd = 100
-    priceAdd += 10 // priceAdd = priceAdd + 10
-    showLabel("After +=", priceAdd)
-
-    var priceSub = 100
-    priceSub -= 10 // priceSub = priceSub - 10
-    showLabel("After -=", priceSub)
-
-    var priceMul = 100
-    priceMul *= 10 // priceMul = priceMul * 10
-    showLabel("After *=", priceMul)
-
-    var priceDiv = 100
-    priceDiv /= 10 // priceDiv = priceDiv / 10
-    showLabel("After /=", priceDiv)
-
-    var priceMod = 100
-    priceMod %= 10 // priceMod = priceMod % 10
-    showLabel("After %=", priceMod)
-}
-
-fun showLabel (labelName: String, value: Int) {
-    println("$labelName : $value")
+    val isKotlinFun = true
+    val isFishTasty = false
+    println(isKotlinFun)   // Outputs true
+    println(isFishTasty)   // Outputs false
 }
