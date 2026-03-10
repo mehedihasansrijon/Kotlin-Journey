33d9d2a28cf0b8c2e1e5a090e2ddfdf4f1733546
Mehedi Hasan Srijon <srijon.the.dreamer@gmail.com>
2026-03-10 11:01:35 +0600

    Person model class

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,9 @@
+import models.Person
+
 fun main() {
-    //    This is single line code
-    println("Hello World!")
-    /*This is a multi
-    line comment.*/
-    println("I am learning Kotlin")
-    println("I am learning English")
-    println(8 + 8 + 8)
+    val person = Person("Mehedi Hasan Srijon", 2007, "Jannan Ali")
+
+    println("Name: ${person.name}")
+    println("Birthyear: ${person.birthYear}")
+    println("Father Name: ${person.fatherName}")
 }
---
+++ src/models/Person.kt
@@ -0,0 +1,7 @@
+package models
+
+data class Person (
+    val name: String,
+    val birthYear: Int,
+    val fatherName : String
+)
