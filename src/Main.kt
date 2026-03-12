63fa3327fe90369759fcb485dfe8603ae4d9468c
Mehedi Hasan Srijon <srijon.the.dreamer@gmail.com>
2026-03-12 19:25:44 +0600

    To create an array, use the arrayOf() function, and place the values in a comma-separated list inside it

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,6 @@
 fun main() {
-
-    var i = 1
-    while (i <= 10) {
-        if (i == 4) {
-            i++
-            continue
-        }
-        println("I am $i")
-        i++
-    }
-
-    var x = 1
-    while (x <= 10) {
-        if (x == 4) {
-            break
-        }
-        println("I am $x")
-        x++
+    var countries = arrayOf("Russia", "USA", "Poland")
+    for (i in countries) {
+        println(i)
     }
 }
