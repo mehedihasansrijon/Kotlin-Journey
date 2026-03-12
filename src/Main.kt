decab00ac8f061e024b1c221994a2fae9883f564
Mehedi Hasan Srijon <srijon.the.dreamer@gmail.com>
2026-03-12 19:39:39 +0600

    Looping Through a Map

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
-    for (i in 10 downTo 1) {
-        println(i)
+    val user = mapOf("Srijon" to 18, "Janet" to 19, "Mehedi" to 28, "Jisa" to 29, "April" to 30)
+    for ((name, age) in user) {
+        println("My name is $name, Age is $age")
     }
 }
