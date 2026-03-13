30230320ce48e42b3b4c19dd115b7cebf14cf8a3
Mehedi Hasan Srijon <srijon.the.dreamer@gmail.com>
2026-03-13 10:06:55 +0600

    If. else best practices

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,6 @@
-import models.Member
-import kotlin.collections.mutableListOf
-import kotlin.contracts.Returns
-
 fun main() {
-    team()
-}
+    val isActive : Boolean = false
 
-fun team() {
-    val memberList = mutableListOf<Member>()
-
-    addMember(memberList, "Srijon", 22)
-    addMember(memberList, "Rahim", 25)
-    addMember(memberList, "Karim", 30)
-    printTeam(memberList)
-}
-
-fun addMember(mutableList: MutableList<Member>, name: String, age: Int) {
-    mutableList.add(Member(name, age))
-}
-
-fun printTeam(mutableList: MutableList<Member>) {
-    for (member in mutableList) {
-        println("Name: ${member.name}, Age: ${member.age}\n")
-    }
+    val activeTitle : String = if (isActive) "I'm active" else "I'm inactive"
+    println(activeTitle)
 }
